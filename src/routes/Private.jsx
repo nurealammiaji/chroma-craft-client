f6fe478920f53b46ed926f98fc1637308c62c393
nurealammiaji <nurealam.miaji@gmail.com>
2024-02-02 22:45:39 +0600

    private route added

--- src/routes/Private.jsx
+++ src/routes/Private.jsx
@@ -1,10 +1,37 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+import { RotatingLines } from "react-loader-spinner";
 
-const Private = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Private = ({ children }) => {
+
+    const { user, loading } = useContext(AuthContext);
+
+    const location = useLocation();
+
+    if (loading) {
+        return <>
+            <div className="text-center">
+                <RotatingLines
+                    visible={true}
+                    height="96"
+                    width="96"
+                    color="grey"
+                    strokeWidth="5"
+                    animationDuration="0.75"
+                    ariaLabel="rotating-lines-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                />
+            </div>
+        </>
+    }
+
+    if (user) {
+        return children;
+    }
+
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>
 };
 
 export default Private;
