0dfde1d96a49c8f693a811baeb68d219186a47ba
nurealammiaji <nurealam.miaji@gmail.com>
2024-02-06 16:33:00 +0600

    social login added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,28 +4,115 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useContext } from "react";
 import { AuthContext } from '../../providers/AuthProvider';
+import Swal from "sweetalert2";
 
 const Login = () => {
 
-    const { googleLogin, emailLogin } = useContext(AuthContext);
+    const { emailLogin, googleLogin, githubLogin, facebookLogin } = useContext(AuthContext);
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
-    const emailLoginHandler = (data) => {
+    const handleEmailLogin = (data) => {
         console.log(data);
+        const email = data.email;
+        const password = data.password;
+        emailLogin(email, password)
+            .then(result => {
+                console.log(result);
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Logged in Successfully !",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: `${error.message.slice(9,)}`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
     }
 
     const handleGoogleLogin = () => {
         googleLogin()
             .then(result => {
                 console.log(result);
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Logged in Successfully !",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
             })
             .catch(error => {
                 console.log(error);
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: `${error.message.slice(9,)}`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+    }
+
+    const handleGithubLogin = () => {
+        githubLogin()
+            .then(result => {
+                console.log(result);
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Logged in Successfully !",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: `${error.message.slice(9,)}`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+    }
+
+    const handleFacebookLogin = () => {
+        facebookLogin()
+            .then(result => {
+                console.log(result);
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Logged in Successfully !",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: `${error.message.slice(9,)}`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
             })
     }
 
     return (
         <div>
+            <br /><br />
             <Helmet>
                 <title>Login || Chroma Craft</title>
                 <link rel="canonical" href="https://chromacraftbd.web.app/" />
@@ -33,7 +120,7 @@ const Login = () => {
             <div className="min-h-screen hero">
                 <div className="p-10 mx-auto my-auto shadow-2xl rounded-3xl md:w-6/12 hero-content">
                     <div className="mx-auto md:w-10/12">
-                        <form onSubmit={handleSubmit(emailLoginHandler)}>
+                        <form onSubmit={handleSubmit(handleEmailLogin)}>
                             <div className="text-center">
                                 <h3 className="text-2xl font-semibold">Login</h3>
                             </div>
@@ -42,13 +129,13 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input {...register("email", { required: true })} type="email" placeholder="email" name="email" className="input input-bordered"/>
+                                <input {...register("email", { required: true })} type="email" placeholder="email" name="email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input {...register("password", { required: true })} type="password" placeholder="password" name="password" className="input input-bordered"/>
+                                <input {...register("password", { required: true })} type="password" placeholder="password" name="password" className="input input-bordered" />
                             </div>
                             <div className="mt-6 form-control">
                                 <button className="btn btn-neutral" type="submit">Login</button>
@@ -69,12 +156,13 @@ const Login = () => {
                         </div>
                         <div className="flex items-center justify-center mt-3">
                             <button onClick={handleGoogleLogin} className="btn btn-outline btn-circle btn-success btn-sm"><TbBrandGoogle className="text-2xl" /></button>
-                            <button className="mx-5 btn-sm btn btn-outline btn-circle text-black hover:text-white hover:bg-black"><TbBrandGithub className="text-2xl" /></button>
-                            <button className="btn btn-outline btn-circle btn-info btn-sm"><TbBrandFacebook className="text-2xl" /></button>
+                            <button onClick={handleGithubLogin} className="mx-5 text-black btn-sm btn btn-outline btn-circle hover:text-white hover:bg-black"><TbBrandGithub className="text-2xl" /></button>
+                            <button onClick={handleFacebookLogin} className="btn btn-outline btn-circle btn-info btn-sm"><TbBrandFacebook className="text-2xl" /></button>
                         </div>
                     </div>
                 </div>
             </div>
+            <br /><br />
         </div >
     );
 };
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,20 +2,134 @@ import { Helmet } from "react-helmet-async";
 import { TbBrandFacebook, TbBrandGithub, TbBrandGoogle } from "react-icons/tb";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import { updateProfile } from "firebase/auth";
 
 const Register = () => {
 
-    const { emailRegister, googleLogin } = useContext(AuthContext);
+    const { emailRegister, googleLogin, githubLogin, facebookLogin } = useContext(AuthContext);
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
-    const emailRegisterHandler = (data) => {
+    const handleEmailRegister = (data) => {
         console.log(data);
+        const email = data.email;
+        const password = data.password;
+        const name = data.name;
+        const photo = data.photo;
+        emailRegister(email, password)
+            .then(result => {
+                console.log(result);
+                const currentUser = result.user;
+                updateProfile(currentUser, {
+                    displayName: name,
+                    photoURL: photo,
+
+                })
+                    .then(result => {
+                        console.log(result);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Registered Successfully !",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+
+            })
+            .catch(error => {
+                console.log(error);
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: `${error.message.slice(9,)}`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
     }
+
+    const handleGoogleLogin = () => {
+        googleLogin()
+            .then(result => {
+                console.log(result);
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Logged in Successfully !",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: `${error.message.slice(9,)}`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+    }
+
+    const handleGithubLogin = () => {
+        githubLogin()
+            .then(result => {
+                console.log(result);
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Logged in Successfully !",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: `${error.message.slice(9,)}`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+    }
+
+    const handleFacebookLogin = () => {
+        facebookLogin()
+            .then(result => {
+                console.log(result);
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Logged in Successfully !",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+            .catch(error => {
+                console.log(error);
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: `${error.message.slice(9,)}`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            })
+    }
+
 
     return (
         <div>
+            <br /><br />
             <Helmet>
                 <title>Register || Chroma Craft</title>
                 <link rel="canonical" href="https://chromacraftbd.web.app/" />
@@ -23,7 +137,7 @@ const Register = () => {
             <div className="min-h-screen hero">
                 <div className="p-10 mx-auto my-auto shadow-2xl md:w-6/12 hero-content rounded-3xl">
                     <div className="mx-auto md:w-10/12">
-                        <form onSubmit={handleSubmit(emailRegisterHandler)}>
+                        <form onSubmit={handleSubmit(handleEmailRegister)}>
                             <div className="text-center">
                                 <h3 className="text-2xl font-semibold">Register</h3>
                             </div>
@@ -52,28 +166,39 @@ const Register = () => {
                                     name="photo"
                                     className="input input-bordered"
                                 />
+                                {errors.photo && <label className="label">
+                                    <span className="text-error">This Field is required !!</span>
+                                </label>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input {...register("email", { required: true })}
+                                <input {...register("email", {
+                                    required: true
+                                })}
                                     type="email"
                                     placeholder="email"
                                     name="email"
                                     className="input input-bordered"
                                 />
+                                {errors.email && <label className="label">
+                                    <span className="text-error">This Field is required !!</span>
+                                </label>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input {...register("password", { required: true })}
+                                <input {...register("password", { pattern: /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8}$/, required: true })}
                                     type="password"
                                     placeholder="password"
                                     name="password"
                                     className="input input-bordered"
                                 />
+                                {errors.password && <label className="label">
+                                    <span className="text-error">This Field is required !!</span>
+                                </label>}
                             </div>
                             <div className="mt-6 form-control">
                                 <button className="btn btn-neutral" type="submit">Register</button>
@@ -93,13 +218,14 @@ const Register = () => {
                             </label>
                         </div>
                         <div className="flex items-center justify-center mt-3">
-                            <button className="btn btn-outline btn-circle btn-success btn-sm"><TbBrandGoogle className="text-2xl" /></button>
-                            <button className="mx-5 btn-sm btn btn-outline btn-circle text-black hover:text-white hover:bg-black"><TbBrandGithub className="text-2xl" /></button>
-                            <button className="btn btn-outline btn-circle btn-info btn-sm"><TbBrandFacebook className="text-2xl" /></button>
+                            <button onClick={handleGoogleLogin} className="btn btn-outline btn-circle btn-success btn-sm"><TbBrandGoogle className="text-2xl" /></button>
+                            <button onClick={handleGithubLogin} className="mx-5 text-black btn-sm btn btn-outline btn-circle hover:text-white hover:bg-black"><TbBrandGithub className="text-2xl" /></button>
+                            <button onClick={handleFacebookLogin} className="btn btn-outline btn-circle btn-info btn-sm"><TbBrandFacebook className="text-2xl" /></button>
                         </div>
                     </div>
                 </div>
             </div>
+            <br /><br />
         </div>
     );
 };
