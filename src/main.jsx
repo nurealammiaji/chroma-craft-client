a2d33fab5292432f61cb37dabd9cb5eac32530db
nurealammiaji <nurealam.miaji@gmail.com>
2024-02-08 19:29:30 +0600

    axios public hook added

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,11 @@ import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './providers/AuthProvider.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={QueryClient}>
+    <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <HelmetProvider>
           <div className='max-w-screen-xl mx-auto'>
