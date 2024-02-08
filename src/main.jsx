9386aab2d16a5f69393321ede18eff74ed6c21c9
nurealammiaji <nurealam.miaji@gmail.com>
2024-02-08 14:49:57 +0600

    query client provider added

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,18 @@ import {
 import Public from './routes/Public.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './providers/AuthProvider.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <HelmetProvider>
-        <div className='max-w-screen-xl mx-auto'>
-          <RouterProvider router={Public} />
-        </div>
-      </HelmetProvider>
-    </AuthProvider>
+    <QueryClientProvider client={QueryClient}>
+      <AuthProvider>
+        <HelmetProvider>
+          <div className='max-w-screen-xl mx-auto'>
+            <RouterProvider router={Public} />
+          </div>
+        </HelmetProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
