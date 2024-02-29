c9fdc34d4ae0334d7c413a940c78c75f094a3199
nurealammiaji <nurealam.miaji@gmail.com>
2024-02-29 15:08:48 +0600

    scroll to top feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { Outlet } from 'react-router-dom'
-import Navbar from './Common/Navbar/Navbar'
-import Footer from './Common/Footer/Footer'
+import { Outlet } from 'react-router-dom';
+import Navbar from './Common/Navbar/Navbar';
+import Footer from './Common/Footer/Footer';
+import ScrollToTop from "react-scroll-to-top";
+import { TbCircleArrowUp } from 'react-icons/tb';
 
 function App() {
 
@@ -11,6 +13,12 @@ function App() {
       <Outlet></Outlet>
       <br /><br />
       <Footer></Footer>
+      <ScrollToTop
+        smooth
+        className='tooltip tooltip-bottom'
+        data-tip='Back to Top'
+        component={<TbCircleArrowUp className='mx-auto my-auto text-3xl' />}
+      />
     </>
   )
 }
