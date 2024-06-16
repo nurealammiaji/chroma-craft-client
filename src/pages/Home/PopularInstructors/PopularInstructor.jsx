de7a9a1e4aa0c1b54730e58adb1f92cc10c3eba6
nurealammiaji <nurealam.miaji@gmail.com>
2024-06-17 02:17:54 +0600

    instructor dynamic info added to the home page

--- src/pages/Home/PopularInstructors/PopularInstructor.jsx
+++ src/pages/Home/PopularInstructors/PopularInstructor.jsx
@@ -1,21 +1,36 @@
 import { TbCircleArrowRight } from "react-icons/tb";
 import { Link } from "react-router-dom";
 import shape from "../../../assets/shapes/art-protrait-03.png"
+import useClasses from "../../../hooks/useClasses";
 
 const PopularInstructor = ({ item }) => {
 
     const { _id, instructor_id, instructor, instructor_email, instructor_image, total_classes, classes_name, category_name, category_id } = item;
 
+    const [classes] = useClasses();
+
+    let instructorClasses;
+    if (classes) {
+        instructorClasses = classes.filter(item => item.instructor_email === instructor_email && item.status === "approved");
+    }
+
+    let instructorCategories;
+    if (instructorClasses) {
+        instructorCategories = instructorClasses.map(item => item.category_name);
+        instructorCategories = [...new Set(instructorCategories)]
+        instructorCategories = instructorCategories.join(", ");
+    }
+
     return (
         <div className="border shadow-xl hover:motion-safe:animate-pulse hover:border-neutral rounded-2xl">
-            <div style={{ background: `url(${shape}) bottom right no-repeat` }} className="w-full card glass">
+            <div style={{ background: `url(${shape}) bottom right no-repeat` }} className="w-full h-full card glass">
                 <figure>
                     <img className="w-full h-[240px]" src={instructor_image} alt={`Image of ${instructor}`} />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">{instructor}</h2>
                     <p className="my-3">{instructor_email}</p>
-                    <p>Expertise: <span className="badge badge-accent">{category_name}</span></p>
+                    <p><span className="font-medium">Expertise:</span> <span className="text-accent">{instructorCategories}</span></p>
                     <div className="justify-start mt-10 card-actions">
                         <Link to={`/instructors/${instructor_id}`}>
                             <button className="btn btn-primary btn-outline">More Info <TbCircleArrowRight className="text-xl" /></button>
